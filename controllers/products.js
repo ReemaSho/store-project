348a02df2c8148f309e99902f8129a9dc5d55587
Reema <Reemaalshohof88@gmail.com>
2021-12-22 14:11:06 +0100

    delete gitAllProductsStatic testing function from controllers/routes,, connect  getAllProducts with my mongoose model and update the function to search for all products or filter products based on query strings

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,22 @@
-const getAllProducts = (req, res) => {
-    res.status(200).json({ msg: "products route" });
+import Product from "../models/products.js";
+
+const getAllProducts = async(req, res) => {
+    const { featured, company, name } = req.query;
+    //I set queryOption equal to an empty string , incase the queryString doesn't exist in my data , fun will search for an empty object and return all products
+    const queryOptions = {};
+
+    if (featured) {
+        queryOptions.featured = featured === "true" ? true : false; // ternary operator to set the value of featured property
+    }
+    if (company) {
+        queryOptions.company = company;
+    }
+    if (name) {
+        //{ $regex} mongoDB query operators docs
+        queryOptions.name = { $regex: name, $options: "i" };
+    }
+    const products = await Product.find(queryOptions);
+    res.status(200).json({ products, nHits: products.length });
 };
 
-const getAllProductsStatic = (req, res) => {
-    res.status(200).json({ msg: "products testing route" });
-};
-
-export default { getAllProducts, getAllProductsStatic };
+export default { getAllProducts };
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,8 @@
 import express from "express";
 const router = express.Router();
 import controllers from "../controllers/products.js";
-const { getAllProducts, getAllProductsStatic } = controllers;
+const { getAllProducts } = controllers;
 
 router.route("/").get(getAllProducts);
-router.route("/static").get(getAllProductsStatic);
 
 export default router;
